0b682d8017a09229856b74de122d535166fcda71
trs4630 <tuanrongshan@gmail.com>
2025-03-28 20:48:32 +0800

    fix: sum up total usage

--- cleanup.php
+++ cleanup.php
@@ -6,8 +6,15 @@
             $db, 
             "DELETE rc FROM radcheck rc
             JOIN radacct ra ON rc.username = ra.username
+            WHERE rc.attribute = 'Max-All-Session'
+            AND SUM(ra.acctsessiontime) > rc.value"
+        );
+        $result = mysqli_query(
+            $db, 
+            "DELETE rc FROM radcheck rc
+            JOIN radacct ra ON rc.username = ra.username
             WHERE rc.attribute = 'Max-All-Traffic'
-            AND ra.acctsessiontime > rc.value"
+            AND SUM(ra.acctinputoctets + ra.acctoutputoctets) > rc.value"
         );
         sleep(5);
     }
