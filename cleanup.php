fabe9887713dc001a3803e538e6beda3de49ac07
trs4630 <tuanrongshan@gmail.com>
2025-03-29 21:40:07 +0800

    fix: delete w/ temp table

--- cleanup.php
+++ cleanup.php
@@ -2,40 +2,39 @@
     $db = mysqli_connect("localhost", "radius", "changeme", "radius");
 
     while (true) {
-        $result = mysqli_query(
-            $db, 
-            "DELETE FROM radcheck 
-            WHERE username IN (
-                SELECT tmp.username
+        $queries = [
+            "CREATE TEMPORARY TABLE del 
+            AS (
+                SELECT DISTINCT tmp.username
                 FROM (
                     SELECT ra.username, SUM(ra.acctsessiontime) AS total_session_time
-                    FROM radcheck rc
-                    JOIN radacct ra ON rc.username = ra.username
-                    WHERE rc.attribute = 'Max-All-Session'
+                    FROM radacct ra
                     GROUP BY ra.username
                 ) tmp
                 JOIN radcheck rc ON rc.username = tmp.username
                 WHERE rc.attribute = 'Max-All-Session' 
                 AND tmp.total_session_time > rc.`value`
-            )"
-        );
-        $result = mysqli_query(
-            $db, 
-            "DELETE FROM radcheck 
-            WHERE username IN (
-                SELECT tmp.username
+            )",
+            "DELETE FROM radcheck WHERE username IN (SELECT username FROM del)",
+            "DROP TEMPORARY TABLE del",
+            "CREATE TEMPORARY TABLE del 
+            AS (
+                SELECT DISTINCT tmp.username
                 FROM (
                     SELECT ra.username, SUM(ra.acctinputoctets + ra.acctoutputoctets) AS total_traffic
-                    FROM radcheck rc
-                    JOIN radacct ra ON rc.username = ra.username
-                    WHERE rc.attribute = 'Max-All-Traffic'
+                    FROM radacct ra
                     GROUP BY ra.username
                 ) tmp
                 JOIN radcheck rc ON rc.username = tmp.username
                 WHERE rc.attribute = 'Max-All-Traffic' 
                 AND tmp.total_traffic > rc.`value`
-            )"
-        );
+            )",
+            "DELETE FROM radcheck WHERE username IN (SELECT username FROM del)",
+            "DROP TEMPORARY TABLE del",
+        ];
+        foreach ($queries as $sql) {
+            $good = mysqli_query($db, $sql);
+        };
         sleep(5);
     }
 ?>
